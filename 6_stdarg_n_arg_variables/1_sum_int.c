eadc20484dd9469a40f4c2d7c417302b71e5ada6
mzolotar <maria.zolotarova@gmail.com>
2024-09-11 12:13:45 +0200

    mod name

--- 6_stdarg_n_arg_variables/1_sum_int.c
+++ 6_stdarg_n_arg_variables/1_sum_int.c
@@ -1,3 +1,4 @@
 /*Ejercicio 1: Suma de números enteros
 Escribe una función que acepte un número indefinido de enteros y devuelva su suma. Usa stdarg.h para manejar los argumentos variables.
 Prototipo: int sum_integers(int count, ...);*/
+
