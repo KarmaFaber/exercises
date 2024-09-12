730e39e77c526e51cc4af3e0de2d7e5adff847d1
mzolotar <maria.zolotarova@gmail.com>
2024-09-12 09:29:57 +0200

    mos exrc 2

--- 6_stdarg_n_arg_variables/2_concat_str.c
+++ 6_stdarg_n_arg_variables/2_concat_str.c
@@ -1,3 +1,27 @@
 /*Ejercicio 2: Concatenación de cadenas
-Escribe una función que acepte un número indefinido de cadenas y devuelva una nueva cadena que sea la concatenación de todas ellas. Usa memoria dinámica para almacenar el resultado.
+Escribe una función que acepte un número indefinido de cadenas 
+y devuelva una nueva cadena que sea la concatenación de todas ellas. 
+Usa memoria dinámica para almacenar el resultado.
 Prototipo: char *concat_strings(int count, ...);*/
+
+#include <stdarg.h>
+#include <stdio.h>
+
+char *concat_strings(int count, ...)
+{
+    va_list args;
+    char *dest;
+
+    va_start(args, count);
+
+
+    va_end(args);
+    return(dest);
+}
+
+int main (void)
+{
+
+
+    return (0);
+}
