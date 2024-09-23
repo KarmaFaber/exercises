a16d49b44397a88a7e296056a1d75d80617c452b
mzolotar <maria.zolotarova@gmail.com>
2024-09-23 12:20:38 +0200

    ejerc 3

--- 6_stdarg_n_arg_variables/2_concat_str.c
+++ 6_stdarg_n_arg_variables/2_concat_str.c
@@ -1,90 +1,86 @@
 /*Ejercicio 2: Concatenación de cadenas
-Escribe una función que acepte un número indefinido de cadenas 
-y devuelva una nueva cadena que sea la concatenación de todas ellas. 
-Usa memoria dinámica para almacenar el resultado -> malloc. ft_strlcat.c ? 
+Escribe una función que acepte un número indefinido de cadenas
+y devuelva una nueva cadena que sea la concatenación de todas ellas.
+Usa memoria dinámica para almacenar el resultado -> malloc. ft_strlcat.c ?
 Prototipo: char *concat_strings(int count, ...);*/
 
 #include <stdarg.h>
 #include <stdio.h>
-#include <string.h>
 #include <stdlib.h>
+#include <string.h>
 
-static int ft_lenght(int count, va_list args)
+static int	ft_lenght(int count, va_list args)
 {
-    int i;
-    int total_len;
-    va_list temp_args; // declaramos va_list como temporal para poder reutilizarla en ft_allocate y ft_write_str
-    
-    i=0;
-    total_len = 0;
-    va_copy(temp_args, args); // copiamos la lista de argumentos para no cunsumirla
-    while (i < count)
-    {
-        total_len += strlen(va_arg(temp_args, char *));
-        i++;
-    }
-    va_end(temp_args);
-    return (total_len);
-}
-static char *ft_allocate(int count, va_list args)
-{
-    
-    int total_len;
-    char *new_array;
-    
-    total_len = ft_lenght(count, args);
-    new_array = (char *)malloc((total_len + 1)*(sizeof(char)));
-    if(!new_array)
-        return (NULL);
-        
-    return (new_array);
-}
+	int	i;
+	int	total_len;
 
-static char *ft_write_str(int count, va_list args)
+	va_list temp_args; // declaramos va_list como temporal para poder reutilizarla en ft_allocate y ft_write_str
+	i = 0;
+	total_len = 0;
+	va_copy(temp_args, args); // copiamos la lista de argumentos para no cunsumirla
+	while (i < count)
+	{
+		total_len += strlen(va_arg(temp_args, char *));
+		i++;
+	}
+	va_end(temp_args);
+	return (total_len);
+}
+static char	*ft_allocate(int count, va_list args)
 {
-    int i;      // args_counter: contador de cadenas que pasaran por args
-    int x;      //indice para new_arrray
-    char *current_str;
-    char *new_array;
-    va_list temp_args;
-    
-    i=0;
-    x=0; 
-    new_array=ft_allocate(count, args);
-    if (!new_array)
-        return (NULL);
-    va_copy(temp_args, args);  
-    while (i < count)
-    {
-        current_str=va_arg(temp_args, char *);
-        while (*current_str)
-            new_array[x++] = *current_str++;
-        i++;
-    }
-    va_end(temp_args);
-    new_array[x]='\0';
-    return (new_array);
+	int		total_len;
+	char	*new_array;
+
+	total_len = ft_lenght(count, args);
+	new_array = (char *)malloc((total_len + 1) * (sizeof(char)));
+	if (!new_array)
+		return (NULL);
+	return (new_array);
 }
 
-char *concat_strings(int count, ...)
+static char	*ft_write_str(int count, va_list args)
 {
-    va_list args;
-    char *result;       //string copiado que decolvemos
+	char	*current_str;
+	char	*new_array;
+	va_list	temp_args;
 
-    va_start(args, count);
-
-    result =ft_write_str(count, args);
-
-    va_end(args);
-    return(result);
+	int i; // args_counter: contador de cadenas que pasaran por args
+	int x; // indice para new_arrray
+	i = 0;
+	x = 0;
+	new_array = ft_allocate(count, args);
+	if (!new_array)
+		return (NULL);
+	va_copy(temp_args, args);
+	while (i < count)
+	{
+		current_str = va_arg(temp_args, char *);
+		while (*current_str)
+			new_array[x++] = *current_str++;
+		i++;
+	}
+	va_end(temp_args);
+	new_array[x] = '\0';
+	return (new_array);
 }
 
-int main (void)
+char	*concat_strings(int count, ...)
 {
-    char *new_array=concat_strings(4, "hola", "ll", "xxxxx", "000000");
-    
-    printf("concat_str: %s\n", new_array); // el primer argumento es el numero de argumentos que vamos a pasar. 
-    
-    free(new_array);
-    return (0); 
+	va_list	args;
+
+	char *result; // string copiado que decolvemos
+	va_start(args, count);
+	result = ft_write_str(count, args);
+	va_end(args);
+	return (result);
+}
+
+int	main(void)
+{
+	char *new_array = concat_strings(4, "hola", "ll", "xxxxx", "000000");
+
+	printf("concat_str: %s\n", new_array);// el primer argumento es el numero de argumentos que vamos a pasar.
+
+	free(new_array);
+	return (0);
 }
--- 6_stdarg_n_arg_variables/3_find_max.c
+++ 6_stdarg_n_arg_variables/3_find_max.c
@@ -1,6 +1,38 @@
 /*Ejercicio 3: Encontrar el máximo de una lista de números
-Escribe una función que acepte una cantidad variable de números enteros y devuelva el valor máximo entre ellos.
+Escribe una función que acepte una cantidad variable de números enteros
+y devuelva el valor máximo entre ellos.
 Prototipo: int find_max(int count, ...);*/
 
+#include <limits.h>
+#include <stdarg.h>
+#include <stdio.h>
 
+int	find_max(int count, ...)
+{
+	int		i;
+	int		max;
+	int		current_arg;
+	va_list	args;
 
+	i = 0;
+	max = INT_MIN;
+	va_start(args, count);
+	while (i < count)
+	{
+		current_arg = va_arg(args, int);
+		if (current_arg > max)
+			max = current_arg;
+		i++;
+	}
+	va_end(args);
+	return (max);
+}
+
+int	main(void)
+{
+	int	max;
+
+	max = find_max(5, 0, 7, -2, 10, 2);
+	printf("max_return: %i\n", max);
+	return (0);
+}
