086cdc79da5ff2ffbc409fc2157624cf32d66ae8
mzolotar <maria.zolotarova@gmail.com>
2024-09-23 12:00:50 +0200

    ejerc2

--- 6_stdarg_n_arg_variables/1_sum_int.c
+++ 6_stdarg_n_arg_variables/1_sum_int.c
@@ -1,5 +1,6 @@
 /*Ejercicio 1: Suma de números enteros
-Escribe una función que acepte un número indefinido de enteros y devuelva su suma. Usa stdarg.h para manejar los argumentos variables.
+Escribe una función que acepte un número indefinido de enteros y devuelva su suma. 
+Usa stdarg.h para manejar los argumentos variables.
 Prototipo: int sum_integers(int count, ...);*/
 
 #include <stdarg.h>
--- 6_stdarg_n_arg_variables/7_count_words_by_len.c
+++ 6_stdarg_n_arg_variables/7_count_words_by_len.c
@@ -1,3 +1,4 @@
 /*Ejercicio 7: Contar palabras con longitud específica
-Escribe una función que acepte una cantidad variable de cadenas y un entero que represente la longitud de una palabra. La función debe devolver cuántas de las cadenas tienen exactamente esa longitud.
+Escribe una función que acepte una cantidad variable de cadenas y un entero que represente la longitud de una palabra. 
+La función debe devolver cuántas de las cadenas tienen exactamente esa longitud.
 Prototipo: int count_words_by_length(int length, int count, ...);*/
