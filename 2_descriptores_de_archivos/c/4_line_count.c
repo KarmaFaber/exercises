6e2aeeb75258c8fb793c4aa8b24d80fb25a8cc72
mzolotar <maria.zolotarova@gmail.com>
2024-09-28 20:08:50 +0200

    ejerc 4

--- 2_descriptores_de_archivos/c/4_line_count.c
+++ 2_descriptores_de_archivos/c/4_line_count.c
@@ -8,44 +8,28 @@ Define un tamaño fijo para las líneas y utiliza un búfer.
 #include <errno.h>          // errno
 #include <stdio.h>          // perror, printf
 #include <fcntl.h>          // O_RDONLY
-#include <string.h>         // memcpy
 
-//en este caso solo contamos el numero de lineas limitado por count_to_read y el buffer de read
-static size_t ft_line_counter (char *buffer, ssize_t bytes_read)
+
+//en este caso contamos todo el buffer. dadno por hecho que lee todo el archivo. Cuenta el numero de '\n' no delineas reales
+static size_t ft_line_counter (char *buffer)
 {
     size_t line_num = 0;
-    int i=0;
+    int i = 0;
 
-    while (i < bytes_read)
+    while (buffer[i])
     {
         if (buffer[i] == '\n')
             line_num++;
         i++;
     }
-    printf("return ft_line_counter: %ld\n", line_num);
+    //printf("return ft_line_counter: %ld\n", line_num);
     return (line_num);
 }
-//en este caso contamos el numero de lineas de todo el archivo, siempre y cuando el buffer sea sufinete para almacenar todo el archivo
-/*static size_t ft_line_counter (char *buffer)
-{
-    size_t line_num = 0;
-    int i=0;
-
-    while (buffer[i])
-    {
-        if (buffer[i]=='\n')
-            line_num++;
-        i++;
-    }
-
-    return (line_num);
-}*/
 
 int main (void)
 {
     int fd;
     ssize_t bytes_readed;
-    size_t count_to_read = 15;
     size_t line_count=0;
     char buffer [1000];
     
@@ -58,8 +42,8 @@ int main (void)
         return 1;
     }
     
-    while ((bytes_readed=read(fd, buffer, count_to_read)) > 0)
-        line_count += ft_line_counter(buffer, bytes_readed); //contar el numero de lineas
+    while ((bytes_readed=read(fd, buffer, sizeof(buffer))) > 0)
+        line_count += ft_line_counter(buffer); //contar el numero de lineas
 
 
     // Manejo de errores en la lectura
@@ -78,7 +62,5 @@ int main (void)
         perror("Error al cerrar el archivo");
         return 1;
     }
-    
-
     return (0);
 }
---
+++ 2_descriptores_de_archivos/c/4_line_count_v1.c
@@ -0,0 +1,185 @@
+/*4.Lectura de líneas: Crea un programa que lea un archivo línea por línea usando read. 
+Define un tamaño fijo para las líneas y utiliza un búfer.
+*/
+
+// gcc -Wall -Wextra -Werror 4_line_count_v1.c -o 4_line_count_v1
+
+#include <unistd.h>         // open, read, write, close
+#include <errno.h>          // errno
+#include <stdio.h>          // perror, printf
+#include <fcntl.h>          // O_RDONLY
+#include <string.h>         // memcpy
+
+#define BUFFER_SIZE 100
+//#define LINE_BUFFER_SIZE 100 
+
+//en este caso solo contamos el numero de lineas limitado por count_to_read y el buffer de read
+static size_t ft_line_counter (char *buffer, ssize_t bytes_read)
+{
+    size_t line_num = 0;
+    int i=0;
+
+    while (i < bytes_read)
+    {
+        if (buffer[i] == '\n')
+            line_num++;
+        i++;
+    }
+    printf("return ft_line_counter: %ld\n", line_num);    //comprobacion de line_num retornadas
+    return (line_num);
+}
+
+int main (void)
+{
+    int fd;
+    ssize_t i = 0;
+    ssize_t bytes_readed;
+    size_t line_count = 0;
+    char buffer [BUFFER_SIZE];                      
+    
+
+    fd = open("0_ex_text.txt", O_RDONLY);
+    // Manejo de errores al abrir
+    if (fd == -1)
+    {
+        perror("Error al abrir el archivo");
+        return 1;
+    }
+    
+    //leer el archivo en bloques y contar las lineas
+    while ((bytes_readed=read(fd, buffer, BUFFER_SIZE)) > 0)
+    {
+        //contar laslineas del bufferleido:
+        line_count += ft_line_counter(buffer, bytes_readed);
+
+        //comprobar si el ultimo caracter de la linea es un salto delinea
+        if (buffer[bytes_readed -1] != '\n')
+        {
+            //mover los caracteres restantes no terminados en linea a la siguiente iteracion 
+            while (i< bytes_readed && buffer[bytes_readed -i -1] != '\n')
+                i++;
+        }
+        else
+            i = 0;        //no hay lineas incompletas
+      
+    }
+
+    // Manejo de errores en la lectura
+    if (bytes_readed == -1)
+    {
+        perror("Error en la lectura del archivo");
+        close(fd);
+        return 1;
+    }
+
+    printf("El número total de lineas es: %ld\n", line_count);
+
+    // Manejo de errores al cerrar
+    if (close(fd) == -1)
+    {
+        perror("Error al cerrar el archivo");
+        return 1;
+    }
+    
+
+    return (0);
+}
+
+
+//version muuuy larga
+/*
+int main (void)
+{
+    int fd;
+    ssize_t bytes_readed;
+    size_t line_count = 0;
+    size_t copy_len = 0;
+    size_t remaining_len = 0;
+    size_t line_buffer_len = 0;              //Longuitud actual del buffer de líneas
+    char buffer [BUFFER_SIZE];                      
+    char line_buffer[LINE_BUFFER_SIZE];     //buffer para lineas incompletas
+
+    
+
+    fd = open("0_ex_text.txt", O_RDONLY);
+    // Manejo de errores al abrir
+    if (fd == -1)
+    {
+        perror("Error al abrir el archivo");
+        return 1;
+    }
+    
+    //leer el archivo en bloques y contar las lineas
+    while ((bytes_readed=read(fd, buffer, BUFFER_SIZE)) > 0)
+    {
+        //procesar las lineas que ya estan en el buffer incompleto(line_buffer)
+        if(line_buffer_len > 0)
+        {
+            copy_len = LINE_BUFFER_SIZE - line_buffer_len;      //espacio disponible en line_buffer
+            if(bytes_readed < copy_len)
+                copy_len = bytes_readed;                        //no copiamos mas de lo que se ha leido
+            memcpy(line_buffer + line_buffer_len, buffer, copy_len); 
+            line_buffer_len += copy_len;
+
+            //contar lineas en el buffer acumulado
+            line_count += ft_line_counter(line_buffer, line_buffer_len);
+
+            //ajustar el buffer para el proximo bloque si aun queda contenido:
+            if (line_buffer[line_buffer_len -1] != '\n' && line_buffer_len == LINE_BUFFER_SIZE)
+            {
+                //si hay contenido incompleto, moverlo al principio del buffer:
+                while (line_buffer_len > 0 && line_buffer[line_buffer_len - 1] != '\n')
+                {
+                    remaining_len ++;
+                    line_buffer_len--;
+                }
+                //mover el contenido uncompleto al principio del buffer
+                memcpy(line_buffer, buffer + (bytes_readed - remaining_len), remaining_len);
+                line_buffer_len = remaining_len;
+            }
+            else
+            {
+                line_buffer_len = 0;
+            }
+
+        }
+
+
+        //contar las lineas del buffer actual 
+        line_count += ft_line_counter(buffer, bytes_readed); //contar el numero de lineas
+
+        //manejar el caso en el que el bloque actual no termina con una nueva linea
+        if (buffer[bytes_readed -1 ] != '\n')
+        {
+            //si no termino con un salto de linea, almacenar los bytes restantes en line_buffer
+            remaining_len =bytes_readed;
+            if (remaining_len > LINE_BUFFER_SIZE)
+                remaining_len = LINE_BUFFER_SIZE;   //limitar al tamaño del buffer
+            memcpy(line_buffer, buffer +  (bytes_readed -remaining_len), remaining_len);
+            line_buffer_len=remaining_len;
+
+        }
+    }
+
+    // Manejo de errores en la lectura
+    if (bytes_readed == -1)
+    {
+        perror("Error en la lectura del archivo");
+        close(fd);
+        return 1;
+    }
+
+    printf("El número total de lineas es: %ld\n", line_count);
+
+    // Manejo de errores al cerrar
+    if (close(fd) == -1)
+    {
+        perror("Error al cerrar el archivo");
+        return 1;
+    }
+    
+
+    return (0);
+}
+*/
+
